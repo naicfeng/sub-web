6ea994ece5940b9cb186f2010740a1f498f8ebbe
naicfeng <blog@weicn.org>
2022-03-24 01:58:50 +0800

    up

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-const PurgecssPlugin = require('purgecss-webpack-plugin');
-const glob = require('glob-all');
 const path = require('path')
 
 function resolve(dir) {
@@ -16,18 +14,6 @@ module.exports = {
     }
   },
   productionSourceMap: false,
-  configureWebpack: {
-    // Merged into the final Webpack config
-    plugins: [
-      new PurgecssPlugin({
-        paths: glob.sync([
-          path.join(__dirname, './src/index.html'),
-          path.join(__dirname, './**/*.vue'),
-          path.join(__dirname, './src/**/*.js')
-        ])
-      }),
-    ]
-  },
   chainWebpack: config => {
     // set svg-sprite-loader
     // 移除 prefetch 插件
