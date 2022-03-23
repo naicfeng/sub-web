03dbe687bd019d3c76ce9d47021dd7b9cb829b40
naicfeng <blog@weicn.org>
2022-03-24 01:24:13 +0800

    optimization size

--- src/plugins/element-ui.js
+++ src/plugins/element-ui.js
@@ -1,20 +1,63 @@
 import Vue from 'vue'
-import Element from 'element-ui'
+import {
+    Loading,
+    MessageBox,
+    Notification,
+    Message,
+    Container,
+    Card,
+    Row,
+    Col,
+    Form,
+    FormItem,
+    Input,
+    Select,
+    Option,
+    OptionGroup,
+    Autocomplete,
+    Checkbox,
+    Popover,
+    Divider,
+    Dialog,
+    Link,
+    Radio,
+    Button,
+
+} from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/zh-CN'
 // import '@/assets/css/element-ui.scss'
 // import '@/assets/css/element-element-variables.scss'
 
-Vue.use(Element, {
-    locale,
-    size: 'small'
-})
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Card);
+Vue.use(Container);
+Vue.use(Checkbox);
+Vue.use(Popover);
+Vue.use(Divider);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(OptionGroup);
+Vue.use(Autocomplete);
+Vue.use(Dialog);
+Vue.use(Link);
+Vue.use(Radio);
+Vue.use(Button);
+//Vue.use(Element, {
+//    locale,
+//    size: 'small'
+//})
 
-Vue.use(Element.Loading.directive);
+Vue.use(Loading.directive);
 
-Vue.prototype.$loading = Element.Loading.service;
-Vue.prototype.$msgbox = Element.MessageBox;
-Vue.prototype.$alert = Element.MessageBox.alert;
-Vue.prototype.$confirm = Element.MessageBox.confirm;
-Vue.prototype.$prompt = Element.MessageBox.prompt;
-Vue.prototype.$notify = Element.Notification;
-Vue.prototype.$message = Element.Message;
+Vue.prototype.$ELEMENT = { locale, size: 'small' };
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
 const path = require('path')
 
 function resolve(dir) {
@@ -5,7 +7,7 @@ function resolve(dir) {
 }
 
 module.exports = {
-  publicPath:  './',
+  publicPath: './',
   css: {
     loaderOptions: {
       less: {
@@ -13,9 +15,56 @@ module.exports = {
       }
     }
   },
-
+  productionSourceMap: false,
+  configureWebpack: {
+    // Merged into the final Webpack config
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './**/*.vue'),
+          path.join(__dirname, './src/**/*.js')
+        ])
+      }),
+    ]
+  },
   chainWebpack: config => {
     // set svg-sprite-loader
+    // 移除 prefetch 插件
+    config.plugins.delete('preload');
+    config.plugins.delete('prefetch');
+    // 压缩代码
+    config.optimization.minimize(true);
+    config
+      .when(process.env.NODE_ENV !== 'development',
+        config => {
+          config
+            .optimization.splitChunks({
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial' // only package third parties that are initially dependent
+                },
+                elementUI: {
+                  name: 'ui', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                },
+                commons: {
+                  name: 'commons',
+                  test: resolve('src/components'), // can customize your rules
+                  minChunks: 3, //  minimum common number
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
+              }
+            })
+          config.optimization.runtimeChunk('single')
+        }
+      )
     config.module
       .rule('svg')
       .exclude.add(resolve('src/icons'))
@@ -31,17 +80,5 @@ module.exports = {
         symbolId: 'icon-[name]'
       })
       .end()
-  },
-
-  pwa: {
-    workboxOptions: {
-      // https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
-      skipWaiting: true,
-      clientsClaim: true,
-      importWorkboxFrom: 'local',
-      importsDirectory: 'js',
-      navigateFallback: '/',
-      navigateFallbackBlacklist: [/\/api\//]
-    }
   }
 };
